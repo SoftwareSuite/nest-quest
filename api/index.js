dd3acb03466d4c13c14824e45060a23735584b66
Usman Ghori <usman.ghori1@icloud.com>
2024-09-26 04:22:00 -0700

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from "./route/user.route.js";
+import authRouter from "./route/auth.route.js"
 
 dotenv.config();
 
@@ -16,8 +17,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, ()=> {
     console.log("server is running on port 3000!");
 });
 
 app.use('/api/user', userRouter )
+app.use('/api/auth', authRouter);
