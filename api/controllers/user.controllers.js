844fc531f88913479248e29844adba86d24a604f
Usman Ghori <usman.ghori1@icloud.com>
2024-10-02 02:49:13 -0700

    add sign out functionality

--- api/controllers/user.controllers.js
+++ api/controllers/user.controllers.js
@@ -45,3 +45,5 @@ export const updateUser = async (req, res, next) => {
         next(error)        
       }
   }
+
+ 
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,9 @@ import {
   deleteUserFailure,
   deleteUserSuccess,
   deleteUserStart,
+  signOutStart,
+  signOutFailure,
+  signOutSuccess,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 export default function Profile() {
@@ -108,7 +111,22 @@ export default function Profile() {
     } catch (error) {
       dispatch(deleteUserFailure(error.message));
     }
-  }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutStart());
+      const res = await fetch('/api/auth/signout')
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signOutFailure(data.message));
+        return;
+      }
+      dispatch(signOutSuccess(data.message));
+    } catch (error) {
+      dispatch(signOutFailure(data.message));
+    }
+  };
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -175,7 +193,9 @@ export default function Profile() {
         <span
         onClick={handleDeleteUser}
         className='text-red-700 cursor-pointer font-custom'>Delete account</span>
-        <span className='text-red-700 cursor-pointer font-custom'>Sign out</span>
+        <span
+        onClick={handleSignOut}
+        className='text-red-700 cursor-pointer font-custom'>Sign out</span>
       </div>
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
       <p className='text-green-700 mt-5 font-custom'>
