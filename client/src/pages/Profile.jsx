d038e8bc9c40e5bd2850c296beb40db676fdc256
Usman Ghori <usman.ghori1@icloud.com>
2024-10-08 05:44:59 -0700

    Complete Show listing functionality.

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -28,6 +28,8 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setshowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
 
   console.log(formData);
@@ -129,6 +131,21 @@ export default function Profile() {
     }
   };
 
+  const handleShowListings = async () => {
+    try {
+      setshowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false){
+        setshowListingsError(true);
+        return;
+      }
+      setUserListings(data)
+    } catch (error) {
+      setshowListingsError(true);
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7 font-custom'>Profile</h1>
@@ -205,6 +222,40 @@ export default function Profile() {
       <p className='text-green-700 mt-5 font-custom'>
         {updateSuccess ? 'User is updated successfully!' : ''}
       </p>
+      <button
+      onClick={handleShowListings}
+      className='font-custom text-green-700 font-semibold w-full'>Show Listings</button>
+      <p className='text-red-700 text-sm font-custom'>{showListingsError ? "Error showing listings" : ""}</p>
+      {userListings &&
+        userListings.length > 0 &&
+        <div className="flex flex-col gap-4">
+          <h1 className='text-center mt-7 text-2xl font-semibold font-custom'>Your Listings</h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className='border rounded-lg p-3 flex justify-between items-center gap-4 font-custom'
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt='listing cover'
+                  className='h-16 w-16 object-contain'
+                />
+              </Link>
+              <Link
+                className='text-slate-700 font-semibold  hover:underline truncate flex-1'
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+  
+              <div className='flex flex-col item-center'>
+                <button className='text-red-700 uppercase font-custom'>Delete</button>
+                <button className='text-green-700 uppercase font-custom'>Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>}
     </div>
   );
 }
