a70374091430c483d8b265a1878148416fd84cf3
Usman Ghori <usman.ghori1@icloud.com>
2024-10-09 03:25:29 -0700

    complete update listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -268,7 +268,9 @@ export default function Profile() {
   
               <div className='flex flex-col item-center'>
                 <button onClick={()=> handleListingDelete(listing._id)} className='text-red-700 uppercase font-custom'>Delete</button>
+                <Link to={`/update-listing/${listing._id}`}>
                 <button className='text-green-700 uppercase font-custom'>Edit</button>
+                </Link>
               </div>
             </div>
           ))}
